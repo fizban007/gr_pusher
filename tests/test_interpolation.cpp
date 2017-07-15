66aca2bd31cbdea56a7b01a6702a3c3d87400209
fizban007 <fizban007@gmail.com>
2017-07-14 22:35:17 -0400

    Added a simple test for the new interpolation scheme

--- tests/test_interpolation.cpp
+++ tests/test_interpolation.cpp
@@ -1,4 +1,5 @@
 #include "algorithms/interpolation.h"
+#include "algorithms/interp_template.h"
 #include "catch.hpp"
 
 using namespace Aperture;
@@ -22,4 +23,13 @@ TEST_CASE("Linear order", "[interpolation]") {
 
   REQUIRE(interp.interp_cell(0.6, 5, 5, 1) - 0.6 == Approx(0.0));
   REQUIRE(interp.interp_cell(0.6, 5, 4, 1) - 0.4 == Approx(0.0));
+}
+
+TEST_CASE("New interpolation", "[interp_template]") {
+  REQUIRE(interp_cell(0.6, 1, 1.0) == Approx(0.1));
+  REQUIRE(interp_cell(0.6, 0, 1.0) == Approx(0.9));
+  REQUIRE(interp_cell(0.1, -1, 1.0) == Approx(0.4));
+
+  REQUIRE(interp_cell(0.6, 0, 1.0, 1) == Approx(0.6));
+  REQUIRE(interp_cell(0.6, -1, 1.0, 1) == Approx(0.4));
 }
