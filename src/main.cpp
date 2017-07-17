b5b5d323c8d9b1a5c876a694d0af1562cd0216cc
fizban007 <fizban007@gmail.com>
2017-07-17 15:43:13 -0400

    Pusher now kinda works

--- src/main.cpp
+++ src/main.cpp
@@ -68,7 +68,7 @@ Fu(int n, const Vec3<double>& u0, const Vec3<double>& x0, int c0,
   Double gamma = Gamma(mid_x, mid_u, mid_cell, grid);
   // std::cout << "Gamma is " << gamma.x() << std::endl;
   Double u_0 = grid.beta(0, mid_cell, mid_x) * mid_u[0] + grid.beta(1, mid_cell, mid_x) * mid_u[1] + grid.beta(2, mid_cell, mid_x) * mid_u[2] - grid.alpha(mid_cell, mid_x) * grid.alpha(mid_cell, mid_x) * gamma;
-  std::cout << "u0 is " << u_0.x() << std::endl;
+  // std::cout << "u0 is " << u_0.x() << std::endl;
 
   Double conn = 0.0;
   for (int i = 1; i < 4; i++) {
@@ -85,7 +85,8 @@ Fu(int n, const Vec3<double>& u0, const Vec3<double>& x0, int c0,
       conn += grid.connection(n, 0, i + 1, mid_cell, mid_x) * mid_u[i] * u_0;
   }
   conn += grid.connection(n, 0, 0, mid_cell, mid_x) * u_0 * u_0;
-  return u[n] - u0[n] - 0.5 * dt * conn / gamma;
+  // return u[n] - u0[n] - dt * conn / (Gamma(x, u, cell, grid) + Gamma(x0, u0, c0, grid));
+  return u[n] - u0[n] - 0.5 * dt * conn / Gamma(mid_x, mid_u, mid_cell, grid);
 }
 
 template <typename Double>
@@ -101,65 +102,41 @@ Fx(int n, const Vec3<double>& u0, const Vec3<double>& x0, int c0,
   auto cell = grid.mesh().get_cell_3d(c);
   auto cell_0 = grid.mesh().get_cell_3d(c0);
   for (int j = 0; j < 3; j++) {
-    result -= dt * grid.inv_metric(n, j, mid_cell, mid_x) * mid_u[j];
+    result -= dt * grid.inv_metric(n, j, mid_cell, mid_x) * (u[j] +  u0[j]);
   }
-  result /= Gamma(mid_x, mid_u, mid_cell, grid);
-  result += x[n] - x0[n] - (cell_0[n] - cell[n]) * grid.mesh().delta[n] + grid.beta(n, mid_cell, mid_x);
+  // std::cout << "gamma u2 is (" << (grid.inv_metric(n, 1, mid_cell, mid_x) * (u0[1] + u[1])).x() << ")" << std::endl;
+  // result /= (Gamma(x, u, c, grid) + Gamma(x0, u0, c0, grid));
+  result /= 2.0 * Gamma(mid_x, mid_u, mid_cell, grid);
+  result += x[n] - x0[n] - (cell_0[n] - cell[n]) * grid.mesh().delta[n] + dt * grid.beta(n, mid_cell, mid_x);
   return result;
 }
 
-int
-main(int argc, char* argv[]) {
-  ////////////////////////////////////////////////////////////////////////////////
-  ///  Setting up the grid and metric we are going to use.
-  ////////////////////////////////////////////////////////////////////////////////
-
-  // The convention for grid parsing is as follows:
-  // Number of cells, Starting coordinate, Total length, Number of guard cells
-  Grid grid(
-      {"DIM1 256 1.0 5.00 1", "DIM2 256 0.0 3.14 1", "DIM3 256 0.0 6.28 1"});
-  auto m = metric::metric_spherical();
-
-  // This line will cache all the necessary quantities on the grid, and save
-  // them into a file. If a file with the same grid/metric configuration is
-  // present, it reads from the file instead.
-  grid.setup_metric(m, grid);
-
-  ////////////////////////////////////////////////////////////////////////////////
-  ///  Initialize particle initial condition.
-  ////////////////////////////////////////////////////////////////////////////////
-  Particle p;
-  p.cell = 128 + 129 * 258 + 24 * 258 * 258;
-  p.u[1] = -1.0;
-  std::cout << "Initially, " << p.x << " " << p.u << " " << p.cell << std::endl;
-
-  ////////////////////////////////////////////////////////////////////////////////
-  ///  Set up iteration
-  ////////////////////////////////////////////////////////////////////////////////
+int iterate_newton(Particle& p, const Grid& grid, double dt) {
   const int max_steps = 100;
-  const double tolerance = 1.0e-8;
-  double dt = 0.001;
-  typedef F<double> Var;
+  const double tolerance = 1.0e-10;
+  typedef F<double, 6> Var;
+  Particle p0 = p;
+  // std::cout << "Initially, " << p0.x << " " << p0.u << " " << p0.cell << std::endl;
   for (int i = 0; i < max_steps; i++) {
-    std::cout << "at step " << i << std::endl;
+    // std::cout << "at step " << i << std::endl;
     // Initialize guess solution
     Vec3<Var> x(p.x[0], p.x[1], p.x[2]);
     Vec3<Var> u(p.u[0], p.u[1], p.u[2]);
-    x[0].diff(0, 6);
-    x[1].diff(1, 6);
-    x[2].diff(2, 6);
-    u[0].diff(3, 6);
-    u[1].diff(4, 6);
-    u[2].diff(5, 6);
+    x[0].diff(0);
+    x[1].diff(1);
+    x[2].diff(2);
+    u[0].diff(3);
+    u[1].diff(4);
+    u[2].diff(5);
     std::array<Var, 6> f;
     int c = p.cell;
     // Compute the residual function
-    f[0] = Fu(0, p.u, p.x, p.cell, u, x, c, grid, dt);
-    f[1] = Fu(1, p.u, p.x, p.cell, u, x, c, grid, dt);
-    f[2] = Fu(2, p.u, p.x, p.cell, u, x, c, grid, dt);
-    f[3] = Fx(0, p.u, p.x, p.cell, u, x, c, grid, dt);
-    f[4] = Fx(1, p.u, p.x, p.cell, u, x, c, grid, dt);
-    f[5] = Fx(2, p.u, p.x, p.cell, u, x, c, grid, dt);
+    f[0] = Fx(0, p0.u, p0.x, p0.cell, u, x, c, grid, dt);
+    f[1] = Fx(1, p0.u, p0.x, p0.cell, u, x, c, grid, dt);
+    f[2] = Fx(2, p0.u, p0.x, p0.cell, u, x, c, grid, dt);
+    f[3] = Fu(0, p0.u, p0.x, p0.cell, u, x, c, grid, dt);
+    f[4] = Fu(1, p0.u, p0.x, p0.cell, u, x, c, grid, dt);
+    f[5] = Fu(2, p0.u, p0.x, p0.cell, u, x, c, grid, dt);
     // Fill the Jacobi matrix
     Eigen::MatrixXd J(6, 6);
     Eigen::VectorXd F(6);
@@ -172,15 +149,16 @@ main(int argc, char* argv[]) {
     // std::cout << J << std::endl;
     // std::cout << F << std::endl;
     // auto J_inv = J.inverse();
-    auto new_f = J.colPivHouseholderQr().solve(F);
-    p.x[0] -= new_f(0);
-    p.x[1] -= new_f(1);
-    p.x[2] -= new_f(2);
-    p.u[0] -= new_f(3);
-    p.u[1] -= new_f(4);
-    p.u[2] -= new_f(5);
+    Eigen::VectorXd new_f = J.colPivHouseholderQr().solve(F);
+    // std::cout << new_f << std::endl;
+    p.x[0] -= new_f[0];
+    p.x[1] -= new_f[1];
+    p.x[2] -= new_f[2];
+    p.u[0] -= new_f[3];
+    p.u[1] -= new_f[4];
+    p.u[2] -= new_f[5];
     auto new_cell = grid.mesh().get_cell_3d(p.cell);
-    for (int j = 0; j < 3; j++) {
+    for (int j = 0; j < grid.dim(); j++) {
       if (p.x[j] >= grid.mesh().delta[j]) {
         new_cell[j] += 1;
         p.x[j] -= grid.mesh().delta[j];
@@ -190,8 +168,57 @@ main(int argc, char* argv[]) {
       }
     }
     p.cell = grid.mesh().get_idx(new_cell[0], new_cell[1], new_cell[2]);
-    if (sqrt(new_f.dot(new_f)) <= tolerance) break;
-    std::cout << p.x << " " << p.u << " " << p.cell << std::endl;
+    // if (sqrt(new_f.dot(new_f)) <= tolerance) break;
+    // std::cout << "Norm is " << new_f.norm() << std::endl;
+    // double u_0 = grid.beta(0, p.cell, p.x) * p.u[0] + grid.beta(1, p.cell, p.x) * p.u[1] + grid.beta(2, p.cell, p.x) * p.u[2] - grid.alpha(p.cell, p.x) * grid.alpha(p.cell, p.x) * Gamma(p.x, p.u, p.cell, grid);
+    // std::cout << p.x << " " << p.u << " " << grid.mesh().get_cell_3d(p.cell) << " " << u_0 << std::endl;
+    if (new_f.norm() <= tolerance) break;
+    if (i == max_steps - 1) return 1;
+  }
+  double u_0 = grid.beta(0, p.cell, p.x) * p.u[0] + grid.beta(1, p.cell, p.x) * p.u[1] + grid.beta(2, p.cell, p.x) * p.u[2] - grid.alpha(p.cell, p.x) * grid.alpha(p.cell, p.x) * Gamma(p.x, p.u, p.cell, grid);
+  auto pos = grid.mesh().pos_particle(p.cell, p.x);
+  std::cout << pos << " " << pos.x * sin(pos.y) <<  " " << p.u << " " << u_0 << std::endl;
+  return 0;
+}
+
+int
+main(int argc, char* argv[]) {
+  ////////////////////////////////////////////////////////////////////////////////
+  ///  Setting up the grid and metric we are going to use.
+  ////////////////////////////////////////////////////////////////////////////////
+
+  // The convention for grid parsing is as follows:
+  // Number of cells, Starting coordinate, Total length, Number of guard cells
+  Grid grid(
+      {"DIM1 256 1.0 5.00 1", "DIM2 256 0.0 3.14 1", "DIM3 256 0.0 6.28 1"});
+      // {"DIM1 256 1.0 5.00 1", "DIM2 256 0.0 3.14 1", "DIM3 1 0.0 6.28 0"});
+  auto m = metric::metric_spherical();
+  //     {"DIM1 256 0.0 2.00 1", "DIM2 256 0.0 3.14 1", "DIM3 256 0.0 6.28 1"});
+  // auto m = metric::metric_log_spherical();
+
+  // This line will cache all the necessary quantities on the grid, and save
+  // them into a file. If a file with the same grid/metric configuration is
+  // present, it reads from the file instead.
+  grid.setup_metric(m, grid);
+
+  ////////////////////////////////////////////////////////////////////////////////
+  ///  Initialize particle initial condition.
+  ////////////////////////////////////////////////////////////////////////////////
+  Particle p;
+  // p.cell = 128 + 129 * 258 + 24 * 258 * 258;
+  p.cell = 128 + 129 * 258 + 24 * 258 * 258;
+  p.u[1] = -1.0;
+  // p.x[0] = 0.5 * grid.mesh().delta[0];
+  // p.x[1] = 0.5 * grid.mesh().delta[1];
+  // p.x[2] = 0.5 * grid.mesh().delta[2];
+
+  ////////////////////////////////////////////////////////////////////////////////
+  ///  Set up iteration
+  ////////////////////////////////////////////////////////////////////////////////
+  const double dt = 0.01;
+  for (int n = 0; n < 1000; n++) {
+    std::cout << "At timestep " << n << std::endl;
+    if (iterate_newton(p, grid, dt) == 1) break;
   }
 
   return 0;
