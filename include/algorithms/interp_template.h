052b819e37183d12f6f634d7367467919a0924a3
fizban007 <fizban007@gmail.com>
2017-07-17 22:31:53 -0400

    Added 3rd order interpolation

--- include/algorithms/interp_template.h
+++ include/algorithms/interp_template.h
@@ -10,7 +10,7 @@ Double
 interp(const Double& dx) {
   // return max(1.0 - sqrt(sqr(dx)), 0.0);
   if (dx < 1.0 && dx >= 0.0) return 1.0 - dx;
-  else if (dx > -1.0 && dx < 0.0) return 1.0 + dx;
+  else if (dx < 0.0 && dx >= -1.0) return 1.0 + dx;
   else return 0.0;
 }
 
@@ -22,6 +22,29 @@ interp_cell(const Double& pos, int cell_diff, double delta, int stagger = 0) {
       (double)cell_diff + (stagger == 0 ? 0.5 : 1.0) - pos / delta;
   return interp(x);
 }
+
+template <typename Double>
+Double
+interp2(const Double& dx) {
+  // return max(1.0 - sqrt(sqr(dx)), 0.0);
+  // if (dx < 1.0 && dx >= 0.0) return 1.0 - dx;
+  // else if (dx > -1.0 && dx < 0.0) return 1.0 + dx;
+  if (dx < 2.0 && dx >= 1.0) return (2.0 - dx) * (2.0 - dx) * (2.0 - dx) / 6.0;
+  else if (dx < 1.0 && dx >= 0.0) return (4.0 - 6.0 * dx * dx + 3.0 * dx * dx * dx) / 6.0;
+  else if (dx < 0.0 && dx >= -1.0) return (4.0 - 6.0 * dx * dx - 3.0 * dx * dx * dx) / 6.0;
+  if (dx < -1.0 && dx >= -2.0) return (2.0 + dx) * (2.0 + dx) * (2.0 + dx) / 6.0;
+  else return 0.0;
+}
+
+template <typename Double>
+Double
+interp2_cell(const Double& pos, int cell_diff, double delta, int stagger = 0) {
+  // cell_diff = target_cell - ptc_cell
+  Double x =
+      (double)cell_diff + (stagger == 0 ? 0.5 : 1.0) - pos / delta;
+  return interp2(x);
+}
+
 }
 
 #endif  // _INTERP_TEMPLATE_H_
